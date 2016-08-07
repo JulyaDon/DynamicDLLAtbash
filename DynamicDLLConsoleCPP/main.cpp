3e359751e52858bad0e8c2737e5cbc756e126569
JulyaDon <don.julya@yandex.ru>
2016-08-08 00:30:59 +0300

    Atbash cipher with dynamic DLL

--- DynamicDLLConsoleCPP/main.cpp
+++ DynamicDLLConsoleCPP/main.cpp
@@ -5,21 +5,25 @@
 
 using namespace std;
 
-typedef char* (*PFN_SayHello)();
+//typedef char* (*PFN_SayHello)();
+typedef char* (*PFN_SayHello)(char*);
 
 int main()
 {
 	HINSTANCE hDynamicDLL;
 
-	if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	//if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	if ((hDynamicDLL = LoadLibrary(TEXT("CipherAtbashDLL"))) == NULL)
 	{
 		
 	}
 	else
 	{
 		PFN_SayHello pfn_SayHello;
-		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
-		cout<<(*pfn_SayHello)()<<endl;
+		//pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
+		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "function");
+		//cout << (*pfn_SayHello)() << endl;
+		cout<<(*pfn_SayHello)("hello")<<endl;
 	}
 
 
